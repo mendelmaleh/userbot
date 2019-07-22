f9b5203f016b4e6974e90990d36b4f2e1f45039d
Mendel E <mendelmaleh@gmail.com>
2019-07-21 23:25:56 -0400

    init v0.2

--- main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/python3 -u
-from pyrogram import Client
-
-plugins = dict(root='plugins')
-app = Client('userbot', plugins=plugins)
-app.run()
--- plugins/shared.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Filters
-from functools import partial
-
-prefix = '.:!'
-separator = None
-
-cmd_filter = partial(Filters.command, prefix=prefix, separator=separator)
