e20f5f046a0ea3d2c12b56fb815fdf872aafcf68
Mendel E <mendelmaleh@gmail.com>
2019-08-11 04:55:34 -0400

    rename shared -> utils, add eff func

--- plugins/utils.py
+++ plugins/utils.py
@@ -6,3 +6,7 @@ chats_allowed = ['pyrogramlounge', -1001257291641]
 
 def gefilter(cmd):
     return Filters.command(cmd, prefixes=p) & (Filters.me | Filters.chat(chats_allowed)) & ~Filters.edited
+
+
+def err(text):
+    return f'<i>{text}</i>'
