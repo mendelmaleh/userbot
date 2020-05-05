1f0dcf687fac1f4f899c73f9d96866971311f7b4
Mendel E <mendelmaleh@gmail.com>
2020-05-05 18:31:30 -0400

    Delete message if welcome was triggered by command

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -10,13 +10,16 @@ welcome = ['👋‎', '✋‎']
 
 @Client.on_message(Filters.new_chat_members & Filters.chat(welcome_chats) | mefilter('hi'))
 async def _(cl, msg):
-    msg = await cl.send_message(
+    w = await cl.send_message(
         chat_id=msg.chat.id,
         text=welcome[0]
     )
 
-    for i in range(2):
-        await awall(asyncio.sleep(1/4), msg.edit(welcome[1]))
-        await awall(asyncio.sleep(1/4), msg.edit(welcome[0]))
+    if msg.from_user.is_self:
+        await msg.delete()
 
-    await awall(asyncio.sleep(2), msg.delete())
+    for i in range(2):
+        await awall(asyncio.sleep(1/4), w.edit(welcome[1]))
+        await awall(asyncio.sleep(1/4), w.edit(welcome[0]))
+
+    await awall(asyncio.sleep(1), w.delete())
