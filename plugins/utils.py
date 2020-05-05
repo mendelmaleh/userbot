ae55ec38363fa42030a1c5285c2eb72845a48884
Mendel E <mendelmaleh@gmail.com>
2020-05-05 17:12:56 -0400

    Parse prefixes and allowed chats from the config

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,12 +1,22 @@
+from configparser import ConfigParser
+
 from pyrogram import Filters
 
-p = list('.:!')
-chats_allowed = ['pyrogramlounge', -1001257291641]
+
+def isnumber(text: str) -> bool:
+    text = text[1:] if text[0] == '-' else text
+    return text.isnumeric()
+
+
+config = ConfigParser()
+config.read("config.ini")
+
+p = list(config['commands'].get('prefixes', '.'))
+allowed = [int(c) if isnumber(c) else c for c in config['commands']['chats'].split()]
 
 
 def gefilter(cmd):
-    return Filters.command(cmd, prefixes=p) & Filters.me & ~Filters.edited
-
+    return Filters.command(cmd, prefixes=p) & (Filters.me | Filters.chat(allowed) & ~Filters.edited)
 
 def err(text):
     return f'<i>{text}</i>'
