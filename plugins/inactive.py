7148f9a163b164db1795e6f3a17cff9a1bad33f0
Mendel E <mendelmaleh@gmail.com>
2020-07-08 14:34:41 -0400

    Add some options to inactive

--- plugins/inactive.py
+++ plugins/inactive.py
@@ -1,17 +1,52 @@
-from pyrogram import Client, Message
-from .utils import mefilter
+'''
+usage: inactive [-h] [-n INACTIVE] [-l LIMIT]
 
+    -h, --help              help message
+    -n <n>, --inactive <n>  number of most inactive users to show [default: 8]
+    -l <l>, --limit <l>     number of members to be retrieved
+'''
+
+from pyrogram import Client, Message
+from docopt import docopt, DocoptExit
+from .utils import edrep, mefilter
+
+import time
 import timeago
 
 
 @Client.on_message(mefilter('inactive'))
 async def _(cl: Client, msg: Message):
-    await msg.edit('...')
+    text = await get_inactive(cl, msg)
+    await edrep(msg, text=text, parse_mode='html')
 
-    await msg.edit(text='\n'.join([
+
+async def get_inactive(cl: Client, msg: Message):
+    try:
+        a = docopt(__doc__, argv=msg.command[1:], help=False)
+    except DocoptExit as e:
+        return f'<pre>{e.usage}</pre>'
+
+    if a['--help']:
+        return f'<pre>{__doc__.strip()}</pre>'
+
+    inactive = int(a['--inactive'])
+    limit = int(a['--limit']) if a['--limit'] else 0
+
+    await msg.edit('...')
+    start = time.time()
+
+    messages = [m
+                async for member in cl.iter_chat_members(msg.chat.id, limit=limit, filter='recent')
+                if not member.user.is_deleted
+                async for m in cl.search_messages(msg.chat.id, limit=1, from_user=member.user.id)]
+
+    delta = time.time() - start
+    messages.sort(key=lambda k: k['date'])
+
+    if inactive > 0:
+        messages = messages[:inactive]
+
+    return '\n'.join([
         f'{m.from_user:mention}\'s last <a href="https://t.me/c/{int(m.chat.id + 100e10) * -1}/{m.message_id}">message</a> was {timeago.format(m.date)}'
-        for m in sorted(
-            [m async for member in cl.iter_chat_members(msg.chat.id) async for m in cl.search_messages(msg.chat.id, limit=1, from_user=member.user.id)],
-            key=lambda k: k['date']
-        )[:5]
-    ]), parse_mode='html')
+        for m in messages
+    ] + [f'<code>{int(delta * 1000)}ms</code>'])
