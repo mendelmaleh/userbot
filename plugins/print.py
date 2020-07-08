34e1a6ef4b2efa4f296bc66c21e8b10ba0c36034
Mendel E <mendelmaleh@gmail.com>
2020-07-08 00:46:58 -0400

    Add more functionality to print

--- plugins/print.py
+++ plugins/print.py
@@ -5,6 +5,13 @@ from .utils import mefilter
 
 
 @Client.on_message(mefilter('print'))
-async def _(_, msg: Message):
+async def _(cl, msg: Message):
     m = msg.reply_to_message or msg # or copy.copy(msg)\n del m.from_user
+
+    if len(msg.command) > 1:
+        if msg.command[1] == 'timestamp':
+            m = m['date']
+        if msg.command[1] == 'chatmember':
+            m = await cl.get_chat_member(m.chat.id, m.from_user.id)
+
     await msg.edit(text='<pre>' + escape(str(m)) + '</pre>', parse_mode='html')
