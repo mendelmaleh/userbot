6d975bfb97946ed3165fb8d88b972e01ad1b6cd3
Mendel E <mendelmaleh@gmail.com>
2021-03-30 18:17:25 -0400

    Add print restrictions option and chat command

--- plugins/print.py
+++ plugins/print.py
@@ -12,7 +12,21 @@ async def _(cl: Client, msg: Message):
     if len(msg.command) > 1:
         if msg.command[1] == 'timestamp':
             m = m['date']
+        if msg.command[1] == 'restrictions':
+            m = m.chat.restrictions or m.forward_from_chat.restrictions
         if msg.command[1] == 'chatmember':
             m = await cl.get_chat_member(m.chat.id, m.from_user.id)
 
+    print(m)
     await msg.edit(text='<pre>' + escape(str(m)) + '</pre>')
+
+
+@Client.on_message(mefilter('chat'))
+async def _(cl: Client, msg: Message):
+    chat_id = msg.command[1]
+    if chat_id.startswith("-100"):
+        chat_id = int(msg.command[1])
+
+    chat = await cl.get_chat(chat_id)
+    print(chat)
+    await msg.edit(text='<pre>' + escape(str(chat)) + '</pre>')
