b78fbbdcac89cee8a934d71e0c6cd6aa4ae327a6
Mendel E <mendelmaleh@gmail.com>
2021-08-18 04:35:47 -0400

    Update print: add markup and entities args, remove chat command

--- plugins/print.py
+++ plugins/print.py
@@ -4,12 +4,16 @@ from .utils import code, mefilter
 
 
 @Client.on_message(mefilter('print'))
-async def _(cl: Client, msg: Message):
+async def _print(cl: Client, msg: Message):
     m = msg.reply_to_message or msg # or copy.copy(msg)\n del m.from_user
 
     if len(msg.command) > 1:
         if msg.command[1] == 'timestamp':
             m = m['date']
+        if msg.command[1] == 'markup':
+            m = m['reply_markup']
+        if msg.command[1] == 'entities':
+            m = m['entities']
         if msg.command[1] == 'restrictions':
             m = m.chat.restrictions or m.forward_from_chat.restrictions
         if msg.command[1] == 'chatmember':
@@ -17,14 +21,3 @@ async def _(cl: Client, msg: Message):
 
     print(m)
     await msg.edit(text=code(m))
-
-
-@Client.on_message(mefilter('chat'))
-async def _(cl: Client, msg: Message):
-    chat_id = msg.command[1]
-    if chat_id.startswith("-100"):
-        chat_id = int(msg.command[1])
-
-    chat = await cl.get_chat(chat_id)
-    print(chat)
-    await msg.edit(text=code(chat))
